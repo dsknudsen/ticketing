44c168b12783c01102f90ce834ac423f137f6cbe
Derek Knudsen <dsknudsen@gmail.com>
2023-01-20 09:14:10 -0800

    created failing test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -10,7 +10,7 @@ it('sets a cookie after successful signin', async () => {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
-    .expect(200);
+    .expect(400);
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
