92202c5a6afd2dcf357ce74d8b763bf4f8db8c7e
Derek Knudsen <dsknudsen@gmail.com>
2023-01-20 09:07:29 -0800

    reconcile changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('JWT secret: ', process.env.JWT_KEY);
+  console.log('Starting up...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT secret key does not exist');
   }
