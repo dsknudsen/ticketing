fce80877309254ad7f42a4d0cbe953b0306b5dc7
Derek Knudsen <dsknudsen@gmail.com>
2023-01-20 17:50:05 -0800

    fixed build-client issue by reverting to original

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,8 @@ export default ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
     return axios.create({
-      baseURL: 'http://ticketing.dev',
-      // baseURL: 'http://ticketing.confluencegroup.io/', - USED FOR EXTERNAL DEPLOYMENT
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers,
     });
   } else {
